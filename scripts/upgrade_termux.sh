fb4d3de07a915ccd0e491c9b29acaf9106df39c6
Ludovic Lamarre <ludo+github@rl82.com>
2022-04-03 17:50:24 -0400

    Added termux config

---
+++ scripts/upgrade_termux.sh
@@ -0,0 +1,12 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Ensure Termux configs are done
+TRMX_CONFIG="$HOME/.termux/termux.properties"
+TRX_CONFIG1="extra-keys = [[]]"
+if [[ "$(grep "^$TRX_CONFIG1" $TRMX_CONFIG | wc -l)" == "0" ]]; then
+  echo "${TRX_CONFIG1}" >> $TRMX_CONFIG
+  echo Just applied $TRX_CONFIG1
+else
+  echo $TRX_CONFIG1 already configured
+fi
+
