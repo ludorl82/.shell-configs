19cc9128ec901fe8581247613dd11c0f2e8d80e1
Ludovic Lamarre <ludo+github@rl82.com>
2022-04-04 19:56:43 -0400

    Refactor of scripts

--- scripts/upgrade_console.sh
+++ scripts/upgrade_console.sh
@@ -59,3 +59,54 @@ if [[ "$(grep "${SSH_CONFIG2:20}" $SSHD_CONFIG | wc -l)" = "0" ]]; then
 else
   echo $SSH_CONFIG2 already configured
 fi
+
+# Sync console configs
+CONFIGS_DIR="$HOME/.shell-configs/configs"
+SCRIPTS_DIR="$HOME/.shell-configs/scripts"
+
+# Vim
+[ ! -d ~/.config/nvim ] && mkdir ~/.config/nvim
+rsync -avh "${CONFIGS_DIR}/.config/nvim/" ~/.config/nvim
+
+# ZSH
+\cp $CONFIGS_DIR/.zshrc* ~/
+[ ! -d ~/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" \
+                      && rm -f install.sh
+[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-vi-mode ] && git clone https://github.com/jeffreytse/zsh-vi-mode.git ~/.oh-my-zsh/custom/plugins/zsh-vi-mode
+if [ ! -d ~/.oh-my-zsh/custom/plugins/kubetail ]; then
+  cd ~/.oh-my-zsh/custom/plugins/
+  git clone https://github.com/johanhaleby/kubetail.git kubetail
+  [ ! -d ~/.local/bin ] && mkdir -p ~/.local/bin
+  cp ~/.oh-my-zsh/custom/plugins/kubetail/kubetail ~/.local/bin
+fi
+
+# FZF
+if [ ! -d ~/.fzf ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+  ~/.fzf/uninstall
+  cd ~/.fzf && git pull && ./install
+fi
+
+# tmux
+\cp $CONFIGS_DIR/.tmux.conf ~/
+\cp $CONFIGS_DIR/.tmux.console.conf ~/
+\cp $CONFIGS_DIR/.tmux.ide.conf ~/
+\cp $CONFIGS_DIR/.tmux.keys.conf ~/
+\cp $CONFIGS_DIR/.tmux.Linux.conf ~/
+\cp $CONFIGS_DIR/.gitmux.conf ~/
+rsync -avh "${CONFIGS_DIR}/.config/tmuxinator/" $HOME/.config/tmuxinator --delete
+
+# SSH
+[ ! -d ~/.ssh/ ] && mkdir -p ~/.ssh/ && ssh-keygen
+chmod 700 ~/.ssh
+if [ ! -f ~/.ssh/authorized_keys ]; then
+  ssh-import-id-gh ludorl82
+fi
+
+# Git
+\cp $CONFIGS_DIR/.gitconfig ~/
+
+# Bash for zsh in WSL
+\cp $CONFIGS_DIR/.bashrc ~/
+\cp $CONFIGS_DIR/.inputrc ~/
