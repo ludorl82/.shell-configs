f4fb663ebfcd14efe8028c3b41ffbcd500f11af5
Ludovic Lamarre <ludo+github@rl82.com>
2022-04-27 17:35:47 -0400

    Fixing zshrc alias

--- scripts/connect_mintty.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Set environment
-export ENV=$1
-export DISPLAY=localhost:0.0
-ssh -Y -o SendEnv=ENV ludorl82@ssh
--- scripts/yank.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-string="$(</dev/stdin)"
-[[ -z "$string" ]] && string="$(tmux save-buffer -)"
-echo -n "$string" | tmux load-buffer -b t -
