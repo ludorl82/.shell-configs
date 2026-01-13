7b6c70fa14776441cfb4b40adb25a81157588eb8
Ludovic Lamarre <34710674+ludorl82@users.noreply.github.com>
2026-01-13 17:33:18 -0500

    Update .console.zshrc.zsh

--- .console.zshrc.zsh
+++ .console.zshrc.zsh
@@ -37,9 +37,9 @@ else
     sed -i 's/CROSS="\\u2718"/CROSS="\\u00d7"/g' $HOME/.zsh/themes/agnoster-zsh-theme/agnoster.zsh-theme
     sed -i 's/GEAR="\\u2699"/GEAR="\\u00f6"/g' $HOME/.zsh/themes/agnoster-zsh-theme/agnoster.zsh-theme
     emojis=(
-      "\\u00AE" "\\u00B6" "\\u00BF" "\\u00C6" "\\u00DE" "\\u00DF"
-      "\\u00B5" "\\u00A9" "\\u00A7" "\\u00A5" "\\u00A4" "\\u00A3"
-      "\\u00A2" "\\u00DF" "\\u00E6" "\\u00B1" "\\u00A1" "\\u00A6"
+      "\\u00AE" "\\u00B6" "\\u00BF" "\\u00C6" "\\u00DE" "\\u00DF" 
+      "\\u00B5" "\\u00A9" "\\u00A7" "\\u00A5" "\\u00A4" "\\u00A3" 
+      "\\u00A2" "\\u00DF" "\\u00E6" "\\u00B1" "\\u00A1" "\\u00A6" 
       "\\u00AA" "\\u00AC"
     )
     RAND_EMOJI_1=$(( $RANDOM % ${#emojis[@]} + 1))
