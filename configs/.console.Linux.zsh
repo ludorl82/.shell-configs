a8c0bdcef618db531f49ec7631f2ba580fd5361d
Ludovic Lamarre <34710674+ludorl82@users.noreply.github.com>
2026-01-13 15:47:25 -0500

    Delete configs/.console.Linux.zsh

--- configs/.console.Linux.zsh
+++
@@ -1,6 +0,0 @@
-if [ -f ~/.local/bin/virtualenvwrapper.sh ] ; then
-  export WORKON_HOME=~/.virtualenvs
-  export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
-  export VIRTUALENVWRAPPER_VIRTUALENV=~/.local/bin/virtualenv
-  source ~/.local/bin/virtualenvwrapper.sh
-fi
