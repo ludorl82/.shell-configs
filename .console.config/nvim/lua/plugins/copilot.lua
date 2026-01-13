a829ef5c9550eafa383e22a4a2a5a914dc68f0d7
Ludovic Lamarre <34710674+ludorl82@users.noreply.github.com>
2026-01-13 16:21:08 -0500

    Delete .console.config/nvim/lua/plugins/copilot.lua

--- .console.config/nvim/lua/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "github/copilot.vim",
-    config = function()
-      -- You can add additional configuration for Copilot here if needed
-      -- For instance, you can customize key bindings, settings, etc.
-    end
-  }
-}
