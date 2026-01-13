0f71fba9b5ecb28019c7e3b87ee5ab82182997d4
Ludovic Lamarre <34710674+ludorl82@users.noreply.github.com>
2026-01-13 15:49:02 -0500

    Create .console.aliases.sh

---
+++ .console.aliases.sh
@@ -0,0 +1,11 @@
+alias ll='ls -alF --color'
+alias grep='grep --color=auto'
+alias mux='~/.shell-scripts/scripts/tmuxinator_wrapper.sh'
+alias vim='nvim'
+alias vi='nvim'
+alias vimdiff='nvim -d'
+
+alias gco='git checkout'
+alias gst='git status'
+
+alias pbcopy="~/.shell-scripts/scripts/pbcopy.sh"
