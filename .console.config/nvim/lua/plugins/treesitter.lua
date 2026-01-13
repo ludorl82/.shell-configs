6acff60b1eeb8e7ab847cb95831b3ead330734e6
Ludovic Lamarre <34710674+ludorl82@users.noreply.github.com>
2026-01-13 16:22:30 -0500

    Create treesitter.lua

---
+++ .console.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate", -- Automatically update parsers after installation
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = { "yaml", "lua", "python", "bash", "json", "markdown", "terraform" }, -- List of languages to install parsers for
+        highlight = {
+          enable = true, -- Enable syntax highlighting
+        },
+        indent = {
+          enable = true, -- Enable indentation based on Treesitter
+        },
+      }
+    end,
+  },
+}
