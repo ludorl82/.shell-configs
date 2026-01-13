d1b761e3f413c825e5e6759acdc079760e73d0ce
Ludovic Lamarre <34710674+ludorl82@users.noreply.github.com>
2026-01-13 16:18:35 -0500

    Create gitgutter.lua

---
+++ .console.config/nvim/lua/plugins/gitgutter.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "airblade/vim-gitgutter",
+    config = function()
+      -- Add any gitgutter-specific configuration here
+      -- For example, you may configure signs or other options
+      vim.g.gitgutter_enabled = 1
+    end
+  }
+}
